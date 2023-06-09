eddd80903017afbef7d3029bd31c0c78f9e9bad0
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-06-09 18:56:20 +0200

    Update GUIMyFrame1.cpp

--- GUIMyFrame1.cpp
+++ GUIMyFrame1.cpp
@@ -3,6 +3,7 @@ GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
 :
 MyFrame1( parent )
 {
+	//definicja trzeba zrobić ENUM
 	timer = new wxTimer();
 	timer->SetOwner(this, 1001);
 
@@ -23,44 +24,41 @@ void GUIMyFrame1::get_started()
 void GUIMyFrame1::Repaint()
 {
 	int a = width / 10.;
-		//rysujemy obrazek po lewej stronie
+		//dzielimy obrazek na kafelki
 	for (int i = 0; i < 9; i++)
 	{
 		for(int j=0;j<14;j++)
 			images[i][j] = (Img_Org.GetSubImage(wxRect(wxPoint(i*a, height-(j+1)*a),wxSize( a, a))));
 	}
-	Img_Cpy = Img_Org.Copy();
-	wxImage Img_Tmp = Img_Cpy.ConvertToGreyscale();
-	    wxBitmap bitmap(Img_Tmp);          // Tworzymy tymczasowa bitmape na podstawie Img_Cpy
-		wxClientDC dc(m_panel4);   // Pobieramy kontekst okna
-		dc.DrawBitmap(bitmap, 0, 0, true); // Rysujemy bitmape na kontekscie urzadzenia
-		//próba podzielenia obrazka na mniejsze czêœci i wyœwietlenia
-		//tutaj trzeba te¿ przekszta³ciæ nasz¹ macierz 
-		
-
-		//wxDateTime now = wxDateTime::Now();
-
+	
+    wxBitmap bitmap(Img_Org);          // Tworzymy tymczasowa bitmape na podstawie Img_Cpy
+    wxClientDC dc(m_panel4);   // Pobieramy kontekst okna
+    dc.DrawBitmap(bitmap, 0, 0, true); // Rysujemy bitmape na kontekscie urzadzenia
 
 }
 void GUIMyFrame1::Timer1_Timer(wxTimerEvent& e)
 {
 	static int i = 0;
 	static int j = 0;
-	static int k = 0;
+	static int k = 0;//zmienna ktora zmienia położenie y kafelka
 	static int x = 0;//mowi czy odwracac czy nie
 	//narazie zak³adam taki rozmiar kwadratu
 	int a = width / 10;
 	//sprawdzamy stan klawiszy
 	timer->Stop();
 	if (wxGetKeyState(WXK_RIGHT))
+		//l=1 przesuwamy kafel w prawo
 		l++;
 	if (wxGetKeyState(WXK_LEFT))
 		l--;
 	if (wxGetKeyState(WXK_UP))
+		//obracamy o 90 stopni
 		x++;
 	if (wxGetKeyState(WXK_DOWN))
+		//obracamy o -90 stopni
 		x--;
 	if (wxGetKeyState(WXK_TAB))
+		//odbicie lustrzane
 		h++;
 	timer->Start();
 
@@ -72,14 +70,16 @@ void GUIMyFrame1::Timer1_Timer(wxTimerEvent& e)
 	WxStaticText1->SetLabel(
 		wxString::Format("%02d", now.GetMinute()-started.GetMinute()) + ":" + 
 		wxString::Format("%02d", sec));
-	//tutaj zak³adam sobie jak¹œ szybkoœæ spadania
+	//tutaj zak³adam sobie jak¹œ szybkoœæ spadania,tik czasowy
 	if ((sec-now_sec)==0,1)
 	{
 		
 			wxClientDC dc_two(m_panel3);
 			dc_two.Clear();
 			if(k==0)
+				//pobieram kopie, aby nie modyfikować oryginału
 				tmp = images[i][j].Copy();
+		//wadą jest to że za każdym razem, za każdą iteracją to obracam-brak optymalizacji
 			if (x < 0) {
 				tmp = tmp.Rotate(-M_PI / 2.0, wxPoint(tmp.GetWidth() / 2.,tmp.GetHeight() / 2.));
 				x = 0;
@@ -97,20 +97,25 @@ void GUIMyFrame1::Timer1_Timer(wxTimerEvent& e)
 				tmp = tmp.Mirror();
 				h = 0;
 			}
+			//rysujemy kafelek
 			dc_two.DrawBitmap(wxBitmap(tmp), width / 2 + l * 10, 10 * k, true);
+			//tu wyświetlam już ułożone kafelki-także błąd optymalizacji-trzeba to zmienić!
 			for (auto r : v)
 			{
 				dc_two.DrawBitmap(r.image, r.x, r.y, true);
 			}
+			//sprawdzam czy kafel trafił na miejsce-albo w okolice-myślę, że też można coś tutaj zmienić
 			if ((width / 2 + l *10)>=(i*a-5) && (width / 2 + l * 10)<=(i * a + 5)
 				&& (10*k) >= (height-j*a-40)&& (k* 10)<=(height - j*a))
 			{
+				//tutaj  sprawdzam czy kwadrat nie jest jakoś odwrócony
 				if (*tmp.GetData() == *images[i][j].GetData())
 				{
-					//gwarantuje, że będzie ładnie ułożone
+					//gwarantuje, że będzie ładnie ułożone-jako współrzędne wpisuję pierwotne współrzędne
 					v.push_back(Kwadrat(wxBitmap(images[i][j]),i*a, height - j * a - 40));
 					k = 0;
 					x = 0;
+					//pobieram kolejny element z tablicy kafelków
 					i++;
 					l = 0;
 				}
@@ -137,23 +142,16 @@ void GUIMyFrame1::Timer1_Timer(wxTimerEvent& e)
 		
 	}
 }
-void GUIMyFrame1::Repaint_left()
-{
 
-	wxImage tmp = Img_Org.GetSubImage(wxRect(sec, sec, 10+sec, 10+sec));
-	wxBitmap bitmap_tmp(tmp);
-	wxClientDC dc_two(m_panel3);
-	dc_two.Clear();
-	dc_two.DrawBitmap(bitmap_tmp,sec , sec, true);
-}
 void GUIMyFrame1::hhh(wxKeyEvent& event) 
 {
+	//to chyba musi być xxd
 }
 
 void GUIMyFrame1::ssize(wxSizeEvent& event) {
 	//narazie tutaj
 	
-	//pobieramy aktualny rozmiar panelu z prawej strony dla dopasowania obrazka
+	//pobieramy aktualny rozmiar panelu z prawej strony dla dopasowania obrazka// m_panel_right
 	height = m_panel4->GetSize().GetHeight();
 	width= m_panel4->GetSize().GetWidth();
 	//resizujemy obrazek
@@ -161,5 +159,6 @@ void GUIMyFrame1::ssize(wxSizeEvent& event) {
 	Img_Org = tmp.Copy();
 	//Img_Org.Resize(m_panel4->GetSize(), m_panel4->GetPosition());
 	Repaint();
+	//nie mam pojęcia gdzie tę funkcję wstawić xd
 	get_started();
 }
