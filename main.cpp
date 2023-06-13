6dd764921110da47b69cb7bb2aa086b72dab40b6
DK333D <64826011+DK333D@users.noreply.github.com>
2023-06-13 10:47:04 +0200

    main.cpp update-tidied,added menuBar

--- main.cpp
+++ main.cpp
@@ -6,34 +6,31 @@ class MyApp : public wxApp {
 
 public:
 
- virtual bool OnInit();
- virtual int OnExit() { return 0; }
+	virtual bool OnInit();
+	virtual int OnExit() { return 0; }
 
 };
 
 IMPLEMENT_APP(MyApp);
 
-bool MyApp::OnInit() 
+bool MyApp::OnInit()
 {
- SetProcessDPIAware();
- GUIMyFrame1 *mainFrame = new GUIMyFrame1(NULL);
-wxImage::AddHandler(new wxJPEGHandler);           // Dodajemy handlery do formatow
- wxImage::AddHandler(new wxPNGHandler);            // z jakich bedziemy korzytsac
- wxImage image;
-     wxLogNull logNo;
-     if (!image.LoadFile("Alicja.jpg"))
-     {
-         wxMessageBox(_("Nie uda\u0142o si\u0119 za\u0142adowa\u0107 obrazka"));
-         mainFrame->Destroy();
-         return true;
-     }
-     else
-     {
-         mainFrame->Img_Org = image.Copy();              // Kopiujemy obrazek do Img_Org
-     }
+	wxImage::AddHandler(new wxJPEGHandler);           // Dodajemy handlery do formatow
+	wxImage::AddHandler(new wxPNGHandler);            // z jakich bedziemy korzytsac
 
- mainFrame->Show(true);
- SetTopWindow(mainFrame);
+	wxImage image;
+	wxLogNull logNo;
+	if (!image.LoadFile("brick.png"))
+	{
+		wxMessageBox(_("Nie uda\u0142o si\u0119 za\u0142adowa\u0107 obrazka"));
+		//mainFrame->Destroy();
+		return true;
+	}
 
- return true;
+	GUIMyFrame1 *mainFrame = new GUIMyFrame1(NULL);
+	mainFrame->Img_Org = image.Copy();              // Kopiujemy obrazek do Img_Org
+	mainFrame->Show(true);
+	SetTopWindow(mainFrame);
+
+	return true;
 }
