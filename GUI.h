0daef43bf603f2562340d199bfe6bc9c8989feff
Alicja Kałuża <92887428+alicja832@users.noreply.github.com>
2023-06-26 22:07:51 +0200

    Update GUI.h

--- GUI.h
+++ GUI.h
@@ -29,17 +29,45 @@ class MyFrame1 : public wxFrame
 {
 	private:
 
-	protected:
-		wxPanel* m_panel3;
-		wxPanel* m_panel4;
-		virtual void ssize(wxSizeEvent& event) { event.Skip(); }
-		virtual void Timer1_Timer(wxTimerEvent& e) { e.Skip(); }
-		virtual void hhh(wxKeyEvent& event) { event.Skip(); }
+		wxRadioButton* radioImage1;
+		wxRadioButton* radioImage2;
+		wxRadioButton* radioImage3;
 
+		wxRadioButton* radioElements1;
+		wxRadioButton* radioElements2;
+		wxRadioButton* radioElements3;
+
+		wxRadioButton* radioTempo1;
+		wxRadioButton* radioTempo2;
+		wxRadioButton* radioTempo3;
+
+	protected:
+
+		wxPanel* m_panel_left;
+		wxPanel* m_panel_right;
+		wxPanel* m_panel_options;
+
+		wxBoxSizer* bSizerOptions;
+		wxStaticText* WxStaticText1;
+
+	// Define choices for each radio button group
+		wxString group1Choices[3] = { "Mountains", "Powershell", "Thuja" };
+		wxString group2Choices[3] = { "2", "3", "4" };
+		wxString group3Choices[3] = { "Slow", "Normal", "Fast" };
+
+		virtual void OnSize(wxSizeEvent& event) { event.Skip(); }
+		virtual void StartTimer(wxTimerEvent& e) { e.Skip(); }
+
+			//odpowiedzialne za menu
+		virtual void OnRadioImage(wxCommandEvent& event) { event.Skip(); }
+		virtual void OnRadioElements(wxCommandEvent& event) { event.Skip(); }
+		virtual void OnRadioTempo(wxCommandEvent& event) { event.Skip(); }
+		virtual void OnButtonStartClicked(wxCommandEvent& event) { event.Skip(); }
 
 	public:
 
 		MyFrame1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = "Tetris", const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(800, 600), long style = wxDEFAULT_FRAME_STYLE | wxTAB_TRAVERSAL);
+
 
 		~MyFrame1();
 
